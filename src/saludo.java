45156bfc2032c0531aa08dba4329ca11af560a00
SahynPedraza <dj.thesahyn19@gmail.com>
2016-01-15 15:26:35 -0600

    00-Saludo

---
+++ src/saludo.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+/**
+ * 
+ * @author Sain Pedraza Guerrero
+ *
+ */
+public class saludo {
+	/**
+	 * 
+	 * @param args metodo main
+	 */
+
+	public static void main(String[] args) {
+		/**
+		 * primer mensaje mediante interfas
+		 */
+	JOptionPane.showMessageDialog(null, "Hola Sain");
+	/**
+	 * 
+	 */
+	
+	System.out.println("Hola desde consola");
+	/**
+	 * segundo mensaje desde consola
+	 */
+	
+	
+	
+	
+	
+	}
+
+}
